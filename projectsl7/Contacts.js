d80fdb8fa47107eb613b2a88ed6efad22395a31f
iftakher-t <67021268+iftakher-t@users.noreply.github.com>
2020-11-19 09:01:02 +0600

    Add files via upload

---
+++ projectsl7/Contacts.js
@@ -0,0 +1,38 @@
+
+class Contacts {
+    constructor(){
+        this.contacts = []
+    }
+    getAllContacts(){
+        return this.contacts
+    }
+
+    getContactsById(id){
+        return this.contacts.find( contact =>contact.id == id )
+    }
+
+    createContact(contact){
+        contact.id = this.contacts.length + 1
+        this.contacts.push(contact)
+        return contact
+    }
+
+    updateContactById( {id, name,email,phone} ){
+        let index = this.contacts.findIndex(contact => contact.id == id)
+       
+        this.contacts[index].name = name || this.contacts[index].name
+        this.contacts[index].email = email || this.contacts[index].email
+        this.contacts[index].phone = phone || this.contacts[index].phone
+       
+        return this.contacts[index]
+    }
+
+    deleteContactById(id){
+        let index = this.contacts.findIndex(contact => contact.id == id)
+        let deletedObj = this.contacts[index]
+        this.contacts = this.contacts.filter(contact=> contact.id != id)
+        return deletedObj
+    }
+}
+const contacts = new Contacts()
+module.exports = contacts
---
+++ projectsl7/contactController.js
@@ -0,0 +1,43 @@
+const contacts =require('./Contacts')
+
+exports.getAllContacts = (req,res)=>{
+    res.json(contacts.getAllContacts())
+}
+
+exports.getContactsById = (req,res)=>{
+    
+    let { id } = req.params
+    id = parseInt(id)
+    let contact = contacts.getContactsById(id)
+    res.json(contact)
+}
+
+exports.createContact = (req,res)=>{
+    let { name ,email, phone} = req.body
+    let contact = contacts.createContact({
+         name ,email,phone
+        })
+        res.json(contact)
+}
+
+exports.updateContactById = (req,res)=>{
+    let { id } = req.params
+    id = parseInt(id)
+    
+    let { name ,email, phone} = req.body
+    let contact = contacts.updateContactById({
+          id,
+          name,
+          email,
+          phone
+        })
+        res.json(contact)
+}
+
+exports.deleteContactById = (req,res)=>{
+    let { id } = req.params
+    id = parseInt(id)
+
+    let contact = contacts.deleteContactById(id)
+    res.json(contact)
+}
